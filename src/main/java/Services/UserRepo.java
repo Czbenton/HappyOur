4802327bfc627ff9c29c2227b98019352318e730
Zach Benton <Zach@Zach.local>
2017-01-23 12:29:55 -0600

    user repo

---
+++ src/main/java/Services/UserRepo.java
@@ -0,0 +1,11 @@
+package Services;
+
+import com.zach.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Zach on 1/23/17.
+ */
+public interface UserRepo extends CrudRepository<User, Integer> {
+    User findFirstByName(String userName);
+}
---
+++ src/main/java/com/zach/User.java
@@ -0,0 +1,89 @@
+package com.zach;
+
+import javax.persistence.*;
+
+/**
+ * Created by Zach on 1/23/17.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private boolean isAdmin = false;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password, boolean isAdmin) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
